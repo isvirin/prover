360ff03b82892dd4a2cea303249a24820211bb6a
krainov <krainov9393@mail.ru>
2017-06-20 22:33:54 +0300

    Исправил баг с якорями и добавил переходы на новые вкладки

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,10 +1,16 @@
 $(document).ready(function(){
     $(".sidebar-nav").on("click","a[target!='_blank']", function (event) {
+        event.preventDefault();
         var id  = $(this).attr('href');
         $("#wrapper").removeClass("toggled");
         if (id && id[0] === '#' && $(id)) {
             var top = $(id).offset().top - 45;
-            $('body,html').animate({scrollTop: top}, 750);
+            $('body,html').animate({
+                scrollTop: top
+            }, 750);
+            setTimeout(function () {
+                window.location = id;
+            }, 750);
         }
     });
 
