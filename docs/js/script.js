248ca593184afe5b45dadf24ae215d39696bcbd3
KrKrainov <k.a.krainov@gmail.com>
2017-06-20 17:38:36 +0300

    Сделал закрытие меню при нажатии на ссылку

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     $(".sidebar-nav").on("click","a[target!='_blank']", function (event) {
         var id  = $(this).attr('href');
+        $("#wrapper").removeClass("toggled");
         if (id && id[0] === '#' && $(id)) {
             var top = $(id).offset().top - 45;
             $('body,html').animate({scrollTop: top}, 750);
