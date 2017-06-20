a1f501b77519abba580298903aa68d37b35af94b
KrKrainov <k.a.krainov@gmail.com>
2017-06-20 17:27:43 +0300

    Исправил якорные ссылки, добавил контакты в footer, убрал justify

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
     $(".sidebar-nav").on("click","a[target!='_blank']", function (event) {
-        event.preventDefault();
         var id  = $(this).attr('href');
         if (id && id[0] === '#' && $(id)) {
             var top = $(id).offset().top - 45;
