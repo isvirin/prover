fa7abfcd79900d07819aac4fa9e32ab8531fb9d9
blashyrkh <blashyrkh@inbox.ru>
2017-11-01 19:34:51 +0300

    Conversion of hex string into big-endian number
    Similar to hex2bin, but the result is right-justified in a buffer. Also,
    fixed a bug in hex2bin (characters 0x80-0xff in the input string caused
    an access with a negative index to the translation table).

--- mvp/client/mobile/hex2bin.h
+++ mvp/client/mobile/hex2bin.h
@@ -8,5 +8,9 @@
 
 int hex2bin(const char *hex, uint8_t *bin, unsigned int byteslen);
 
+// A number in hex form (with or without 0x prefix) is decoded into
+// binary form and right-justified in the buffer.
+int hex2bin_bigendian(const char *hex, uint8_t *bin, unsigned int byteslen);
+
 #endif
 
