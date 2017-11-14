ba4ffad698e036bb2312e1e6fec6d8bbadf3b64c
Babay <babay@mail.ru>
2017-11-14 16:51:58 +0300

    #37939 -- added Etherium code for transactions

--- mvp/client/prover_mvp_android/app/src/main/java/io/prover/provermvp/util/Etherium.java
+++ mvp/client/prover_mvp_android/app/src/main/java/io/prover/provermvp/util/Etherium.java
@@ -5,32 +5,20 @@ import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.util.Log;
 
-import org.spongycastle.jcajce.provider.asymmetric.ec.BCECPrivateKey;
+import org.ethereum.crypto.ECKey;
 import org.spongycastle.jcajce.provider.asymmetric.ec.EtherKeccakOutputStream;
 import org.spongycastle.jcajce.provider.asymmetric.ec.EtherKeccakSigner;
-import org.spongycastle.jce.ECNamedCurveTable;
 import org.spongycastle.jce.provider.BouncyCastleProvider;
-import org.spongycastle.jce.spec.ECNamedCurveParameterSpec;
-import org.spongycastle.jce.spec.ECPrivateKeySpec;
-import org.spongycastle.jce.spec.ECPublicKeySpec;
-import org.spongycastle.math.ec.ECPoint;
 
 import java.io.IOException;
 import java.math.BigInteger;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
-import java.security.KeyFactory;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
-import java.security.PrivateKey;
-import java.security.PublicKey;
 import java.security.SecureRandom;
 import java.security.Security;
 import java.security.SignatureException;
-import java.security.spec.ECGenParameterSpec;
-import java.security.spec.InvalidKeySpecException;
 
 import static io.prover.provermvp.Const.TAG;
 
@@ -50,7 +38,7 @@ public class Etherium {
         Security.insertProviderAt(provider, 1);
     }
 
-    KeyPair keyPair;
+    ECKey keyPair;
 
     public Etherium(Context context) {
         keyPair = loadKey(context);
@@ -102,41 +90,30 @@ public class Etherium {
         return false;
     }
 
-    public KeyPair generateKeyPair() throws NoSuchAlgorithmException, NoSuchProviderException, InvalidAlgorithmParameterException {
-        ECGenParameterSpec ecSpec = new ECGenParameterSpec("secp256k1");
+    public ECKey generateKeyPair() throws NoSuchAlgorithmException, NoSuchProviderException, InvalidAlgorithmParameterException {
+        return new ECKey(new SecureRandom());
+        /*ECGenParameterSpec ecSpec = new ECGenParameterSpec("secp256k1");
         KeyPairGenerator keyGen = KeyPairGenerator.getInstance("ECDsA", "SC");
         keyGen.initialize(ecSpec, new SecureRandom());
-        return keyGen.generateKeyPair();
+        return keyGen.generateKeyPair();*/
     }
 
     private void saveKey(Context context) {
-        if (keyPair != null && keyPair.getPrivate() instanceof BCECPrivateKey) {
-            BCECPrivateKey privateKey = (BCECPrivateKey) keyPair.getPrivate();
-            BigInteger d = privateKey.getD();
+        if (keyPair != null) {
+            BigInteger d = keyPair.getPrivKey();
             String key = new String(Base64.encodeBase64(d.toByteArray()));
             PreferenceManager.getDefaultSharedPreferences(context).edit().putString(KEY_PRIVATE, key).apply();
         }
     }
 
-    private KeyPair loadKey(Context context) {
+    private ECKey loadKey(Context context) {
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
         if (!prefs.contains(KEY_PRIVATE))
             return null;
 
         String value = prefs.getString(KEY_PRIVATE, null);
         byte[] bytes = Base64.decode(value.toCharArray());
-        BigInteger d = new BigInteger(bytes);
 
-        try {
-            KeyFactory factory = KeyFactory.getInstance("ECDSA", "SC");
-            ECNamedCurveParameterSpec ec = ECNamedCurveTable.getParameterSpec("secp256k1");
-            PrivateKey privateKey = factory.generatePrivate(new ECPrivateKeySpec(d, ec));
-            ECPoint p = ec.getG().multiply(d);
-            PublicKey publicKey = factory.generatePublic(new ECPublicKeySpec(p, ec));
-            return new KeyPair(publicKey, privateKey);
-        } catch (NoSuchProviderException | NoSuchAlgorithmException | InvalidKeySpecException e) {
-            Log.e(TAG, e.getMessage(), e);
-        }
-        return null;
+        return ECKey.fromPrivate(bytes);
     }
 }
