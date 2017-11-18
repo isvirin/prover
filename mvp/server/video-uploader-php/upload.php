fdf3d706fe5866d4f5a7ddd793564dfb46027744
Alexey Rytikov <user.raa@gmail.com>
2017-11-18 10:20:19 +0300

    Exit with JSON on error. getBlockByHash function (return blocks). Analityc function stub.

--- mvp/server/video-uploader-php/upload.php
+++ mvp/server/video-uploader-php/upload.php
@@ -11,6 +11,81 @@ define('USER_ADDRESS_FILTER', null);
 define('EXAMPLE_FILE_HASH', 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855');
 define('TRANSACTIONBYHASH_CORRECT_INPUT', '0x74305b38');
 
+function uploadResult($isSuccess, $transactions, $error)
+{
+    return json_encode([
+        'success' => $isSuccess,
+        'transactions' => $transactions,
+        'error' => $error
+    ], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+}
+
+function shutdown()
+{
+    $lastError = error_get_last();
+    $error = "Server internal error: {$lastError['message']} ({$lastError['line']})";
+    echo uploadResult(false, [], $error);
+}
+
+// для вывода результата строго в JSON делаем фокус
+error_reporting(0);
+register_shutdown_function('shutdown');
+
+function callAnalyticProgramm($blockHash, $hash)
+{
+    $validated = false;
+    $swype = 0;
+    $beginSwypeTime = 0;
+    $endSwypeTime = 0;
+    //exec(program) and get variables
+    // TODO: для окончательного подтверждения мы должны еще прогнать видео через утилитку
+    return [
+        'validated' => $validated,
+        'swype' => $swype,
+        'beginSwypeTime' => $beginSwypeTime,
+        'endSwypeTime' => $endSwypeTime
+    ];
+}
+
+/**
+ * @param JsonRpc\Client $gethClient
+ * @param string $hash
+ * @return array
+ */
+function getBlockByHash(&$gethClient, $hash)
+{
+    $block = [];
+    if ($gethClient->call('eth_getBlockByHash', [
+        $hash,
+        false
+    ])) {
+        /* Example object(stdClass)
+        difficulty: "0x99379ead"
+        extraData: "0xd783010700846765746887676f312e372e34856c696e7578"
+        gasLimit: "0x665333"
+        gasUsed: "0x3aa59"
+        hash: "0xcb127b04b583a70919e1550fb49139586a5b6d02f0156f4464878201c3106989"
+        logsBloom: "0x00000000000080000000000000000000000000080000000000000000400000000000000000000000000000000000000080000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000000000000000000000000000000004000020000000000000000000000000000000000000000800000000000000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000800000000002000000000000100000000000000000000000000000"
+        miner: "0x8c60d40a2e848251d139fc2b0b6b770bb3351ffd"
+        mixHash: "0xfc438bb77786f3f5921343ecfc227a80461a8c9e6c5831ba5f3ceca8a7d3ab4b"
+        nonce: "0x4fa8ce0eeb1c72cb"
+        number: "0x1f4c1e"
+        parentHash: "0x2b4bc64b05e67c5dedf646e3fa6e685175a93780af6b7dd138759d5e234c5ddf"
+        receiptsRoot: "0x8c1f2693dd7cc19ea03235c1cd6312ce076101088db6b64814128009e1d076f6"
+        sha3Uncles: "0x1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347"
+        size: "0x773"
+        stateRoot: "0x991d6513378d8517860a3e06768292d99954d60bfebb06c9ff58e6851918fe47"
+        timestamp: "0x5a07199a"
+        totalDifficulty: "0x17b50790497fd8"
+        transactions: (11) ["0x9ec75201bcd7dbea8898f82b7ba1bfb18c5d89e9b64feeee602060ef60da008b", "0xe43e32f2866d5cc53b4f24996d62d1dde11698d1b7867c4950cb6b5efd8de4a0", "0xb078678716ddf8a8873d64d3771ec3584bb29f59c56a6efc5019988b7733f8df", "0xfadd9dd5cef89da914bd0a02fa18dd2f1ac28b73d469088b503e8c97b3f7e8c4", "0x73dffe907111577b98b16733b51e0a2f350f67905c14d3eb30f2f3651f083b77", "0x60717d5fc26db71b6817b6db5a1bd112b0eada77eaefd1c2e5b94d401401b98b", "0xbb42cc66fe2d4ddcdf3bc2f3e5e18f2bd7c587c979bf75055312a1d21ade4bf3", "0x9381b9c011083a7e8030e2871fc13b2eac3d7bf51525227aa4f637db1a44f8db", "0x9a7c6953a42de9f6b6b6cf4a7d5834587a3001a8d21c3777eb49633d24b442dc", "0xd05f9167ba17253b1972c1bc5677d908a11586a9f1b0d26f77d4d7d9aaf77810", "0x509b34abb056cbde1244eec58e094511cecd48c14aac3594d5f4031001b0bfd4"]
+        transactionsRoot: "0x0eebcecdbb90d7917d190a5491006e5bad2b465cd61e789e1ba4353950d407b5"
+        uncles: []
+         */
+        $block = $gethClient->result;
+    }
+    return $block;
+}
+
 $isSuccess = false;
 $error = 'error text';
 $transactions = [];
@@ -57,6 +132,12 @@ if (!$mvpHelloInfo['contractAddress']) {
             $data = $transaction->data;
             $senderAddress = $transaction->topics[1];
             $validated = false;
+            $swype = 0;
+            $beginSwypeTime = 0;
+            $endSwypeTime = 0;
+
+            $beginBlock = getBlockByHash($gethClient, $transaction->blockHash);
+            $endBlock = [];
 
             if ($gethClient->call('eth_getTransactionByHash', [
                 $data
@@ -80,14 +161,18 @@ if (!$mvpHelloInfo['contractAddress']) {
                   ["s"]=> string(65) "0x12f49c4ac822f7914a6fcdcca9d44e73fdfd4f5dfbab3e36abbdb0bca5c784c"
                 }
                 */
+                $endBlock = getBlockByHash($gethClient, $transactionDetails->blockHash);
                 if (
                     preg_replace('/0x[0]*(.*)/', '$1', $transactionDetails->from) ===
                     preg_replace('/0x[0]*(.*)/', '$1', $senderAddress)
                 ) {
                     if ($transactionDetails->input === TRANSACTIONBYHASH_CORRECT_INPUT) {
                         if ($transactionDetails->blockHash) {
-                            $validated = true;
-                            // TODO: для окончательного подтверждения мы должны еще прогнать видео через утилитку
+                            $analyticResult = callAnalyticProgramm($transactionDetails->blockHash, $transactionDetails->hash);
+                            $validated = $analyticResult['validated'];
+                            $swype = $analyticResult['swype'];
+                            $beginSwypeTime = $analyticResult['beginSwypeTime'];
+                            $endSwypeTime = $analyticResult['endSwypeTime'];
                         }
                     }
                 }
@@ -95,16 +180,19 @@ if (!$mvpHelloInfo['contractAddress']) {
 
             $transactions[] = [
                 'senderAddress' => $senderAddress,
-                'validated' => $validated
+                'validated' => $validated,
+                'beginBlock' => $beginBlock,
+                'endBlock' => $endBlock,
+                'swype' => $swype,
+                'beginSwypeTime' => $beginSwypeTime,
+                'endSwypeTime' => $endSwypeTime
             ];
+
+            break; // будем проверять только первую найденную транзакцию
         }
     } else {
         $error = $gethClient->error;
     }
 }
 
-die(json_encode([
-    'success' => $isSuccess,
-    'transactions' => $transactions,
-    'error' => $error
-], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
+die(uploadResult($isSuccess, $transactions, $error));
