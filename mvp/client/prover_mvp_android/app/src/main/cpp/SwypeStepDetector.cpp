af98b611cdc4c77c415cb46c00671d764bc4cce8
Babay <babay@mail.ru>
2017-12-08 04:11:14 +0300

    #37939 -- minor changes

--- mvp/client/prover_mvp_android/app/src/main/cpp/SwypeStepDetector.cpp
+++ mvp/client/prover_mvp_android/app/src/main/cpp/SwypeStepDetector.cpp
@@ -14,12 +14,6 @@ void SwypeStepDetector::Add(VectorExplained other) {
     _count++;
 }
 
-float pointDistance(float x1, float y1, float x2, float y2) {
-    float dx = x2 - x1;
-    float dy = y2 - y1;
-    return sqrtf(dx * dx + dy * dy);
-}
-
 void SwypeStepDetector::Reset() {
     VectorExplained::Reset();
     _count = 0;
