bd585634c5677a3a8295b7dd8a1e1562a2188d1a
Babay <babay@mail.ru>
2017-12-09 06:54:42 +0300

    #37939 -- update to use new swipe code response

--- mvp/client/prover_mvp_android/app/src/main/java/io/prover/provermvp/transport/responce/SwypeResponce2.java
+++ mvp/client/prover_mvp_android/app/src/main/java/io/prover/provermvp/transport/responce/SwypeResponce2.java
@@ -1,5 +1,6 @@
 package io.prover.provermvp.transport.responce;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -9,13 +10,22 @@ import org.json.JSONObject;
 
 public class SwypeResponce2 {
     public final String swypeCode;
+    public final int swypeId;
+
+    //{"result": {"swype-id": 16164, "swype-sequence": [5, 6, 8, 9, 8, 5, 1, 2]}}
 
     public SwypeResponce2(String source) throws JSONException, TemporaryDenyException {
         JSONObject obj = new JSONObject(source);
         if (obj.isNull("result")) {
             throw new TemporaryDenyException("Responce in not ready yet");
         }
-        int swypeCode = obj.getInt("result");
-        this.swypeCode = Integer.toString(swypeCode);
+        JSONObject resultJso = obj.getJSONObject("result");
+        swypeId = resultJso.getInt("swype-id");
+        JSONArray swipeSequence = resultJso.getJSONArray("swype-sequence");
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < swipeSequence.length(); i++) {
+            builder.append(swipeSequence.getInt(i));
+        }
+        swypeCode = builder.toString();
     }
 }
