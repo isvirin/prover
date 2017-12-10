2ca950e4af971e60b29e383e1a93330beef8bd90
Babay <babay@mail.ru>
2017-12-10 15:05:33 +0300

    #37939 -- don't use android logger in cpp code if no android

--- mvp/client/prover_mvp_android/app/src/main/cpp/common.h
+++ mvp/client/prover_mvp_android/app/src/main/cpp/common.h
@@ -4,10 +4,18 @@
 
 #define APPNAME "ProverMVP"
 
+#ifdef __ANDROID_API__
+
 #include <android/log.h>
 
 #define LOGI_NATIVE(...) ((void)__android_log_print(ANDROID_LOG_INFO, APPNAME, __VA_ARGS__))
 #define LOGE_NATIVE(...) ((void)__android_log_print(ANDROID_LOG_ERROR, APPNAME, __VA_ARGS__))
+
+#else
+#define LOGI_NATIVE(...) ;
+#define LOGE_NATIVE(...) ;
+
+#endif
 
 #define JNI_COMMIT_AND_RELEASE 0
 
