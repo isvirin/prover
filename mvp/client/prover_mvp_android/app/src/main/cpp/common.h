0d1f54274dd59697914a205b3274acddd5aff0f4
Babay <babay@mail.ru>
2017-12-31 02:18:58 +0300

    ProverMVP -- fix

--- mvp/client/prover_mvp_android/app/src/main/cpp/common.h
+++ mvp/client/prover_mvp_android/app/src/main/cpp/common.h
@@ -3,13 +3,13 @@
 
 #define APPNAME "ProverMVPDetector"
 
+//#define RECT_DEFECT
+#define DEFECT 0.2
+
 #ifdef __ANDROID_API__
 
 #include <android/log.h>
 
-//#define RECT_DEFECT
-
-#define DEFECT 0.2
 
 #define LOGI_NATIVE(...) ((void)__android_log_print(ANDROID_LOG_INFO, APPNAME, __VA_ARGS__))
 #define LOGE_NATIVE(...) ((void)__android_log_print(ANDROID_LOG_ERROR, APPNAME, __VA_ARGS__))
