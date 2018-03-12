9db4e2940646e90327630c18c94ae14f4ff462b8
kirill shakhansky <bb4bbb@gmail.com>
2018-03-12 11:06:02 +0300

    remove debug prints

--- prover_ios/PROVERClapperboard/PROVERClapperboard/QRCode/QRCodeViewController.swift
+++ prover_ios/PROVERClapperboard/PROVERClapperboard/QRCode/QRCodeViewController.swift
@@ -4,13 +4,11 @@ class QRCodeViewController: UIViewController {
 
   // MARK: - IBAction
   @IBAction func closeButtonAction(_ sender: UIButton) {
-    print("close button touched")
     dismiss(animated: true, completion: nil)
   }
   
   // MARK: - Lifecycle
   override func viewDidLoad() {
     super.viewDidLoad()
-    print("view did load")
   }
 }
