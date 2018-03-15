a649a2bfc6da2ce7b5638b47e5e35338b0218f50
kirill shakhansky <bb4bbb@gmail.com>
2018-03-15 13:44:49 +0300

    implement back button

--- prover_ios/PROVERClapperboard/PROVERClapperboard/Screeens/ExportWallet/ExportWalletViewController.swift
+++ prover_ios/PROVERClapperboard/PROVERClapperboard/Screeens/ExportWallet/ExportWalletViewController.swift
@@ -1,4 +1,9 @@
 import UIKit
 
 class ExportWalletViewController: UITableViewController {
+  
+  // MARK: - IBAction
+  @IBAction func backButtonAction(_ sender: UIBarButtonItem) {
+    navigationController?.popViewController(animated: true)
+  }
 }
