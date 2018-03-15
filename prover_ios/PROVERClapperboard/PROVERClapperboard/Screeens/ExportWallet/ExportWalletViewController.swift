79f1981e09c78aecbca5c2c2f314048a7c596258
kirill shakhansky <bb4bbb@gmail.com>
2018-03-15 13:55:00 +0300

    implement show/hide password

--- prover_ios/PROVERClapperboard/PROVERClapperboard/Screeens/ExportWallet/ExportWalletViewController.swift
+++ prover_ios/PROVERClapperboard/PROVERClapperboard/Screeens/ExportWallet/ExportWalletViewController.swift
@@ -8,10 +8,32 @@ class ExportWalletViewController: UITableViewController {
       walletAddress.text = store.ethereumService.hexAddress
     }
   }
+  @IBOutlet weak var showHidePasswordButton: UIButton!
+  @IBOutlet weak var passwordTextField: UITextField!
   
   // MARK: - IBAction
   @IBAction func backButtonAction(_ sender: UIBarButtonItem) {
     navigationController?.popViewController(animated: true)
+  }
+  
+  @IBAction func endInputText(_ sender: UITextField) {
+  }
+  
+  @IBAction func showHidePasswordButtonAction(_ sender: UIButton) {
+    isPasswordSecured = !isPasswordSecured
+  }
+  
+  // MARK: - Private properties
+  var isPasswordSecured = true {
+    didSet {
+      passwordTextField.isSecureTextEntry = isPasswordSecured
+      switch isPasswordSecured {
+      case true:
+        showHidePasswordButton.setImage(#imageLiteral(resourceName: "show"), for: .normal)
+      case false:
+        showHidePasswordButton.setImage(#imageLiteral(resourceName: "hide"), for: .normal)
+      }
+    }
   }
   
   // MARK: - Dependency
